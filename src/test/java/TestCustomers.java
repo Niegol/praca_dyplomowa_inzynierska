56b0b96c3fb05a09af4adcdf85baa337fe4838e1
Maciej Niegolewski <maciejniegolewski@Maciejs-MacBook-Pro.local>
2018-02-07 14:07:53 +0100

    Proba testow mniej pom

--- src/test/java/TestCustomers.java
+++ src/test/java/TestCustomers.java
@@ -1,28 +1,15 @@
 import Controllers.DataBase.Converters.CustomerConverter;
 import Controllers.DataBase.FXModels.CustomerFX;
 import Controllers.DataBase.models.Customer;
-import Controllers.Screens.AllCustomersController;
-import javafx.application.Platform;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.embed.swing.JFXPanel;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.stage.Stage;
-import org.junit.Assert;
 import org.junit.Test;
-import org.junit.runner.RunWith;
 import org.loadui.testfx.GuiTest;
-
 import java.io.IOException;
-
 import static junit.framework.TestCase.assertEquals;
-import static junit.framework.TestCase.assertFalse;
-import static org.testfx.api.FxAssert.verifyThat;
-import static org.testfx.matcher.base.NodeMatchers.hasText;
 
 
 public class TestCustomers extends GuiTest{
